d5b505f0ddc1ea8630e064663e31d935bc4851fd
Augusto Ostapechen <augusto_ostapechen@hotmail.com>
2022-05-06 15:58:09 -0300

    inital commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { useState } from 'react'
+
+function App() {
+  return (
+    <div className="flex justify-center items-center bg-blue-400 rounded-md w-32 h-16">
+      <h1>Hello World</h1>
+    </div>
+  )
+}
+
+export default App
